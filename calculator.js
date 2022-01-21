f9a6838e5d12ae7d44ed5b2510dd555d3b29c714
shubhambhalerao <shubhambhalerao54312@gmail.com>
2022-01-21 13:02:27 +0530

    css_update

--- calculator.js
+++ calculator.js
@@ -1,63 +1,11 @@
-class calculate {
-    constructor(inputval) {
-        this.inputval = inputval;
-        this.clear()
+function display(){
+    let numberbtn = document.getElementsByClassName('btn')
+    let screen = document.getElementById('screen_c')
+    let oprea = document.getElementsByClassName('opre')
 
-    }
-    clear() {
-
-        this.input_value = "";
-        this.operation = undefined;
-    }
-    appendnumber(number) {
-        this.input_value = this.input_value + number;
-
-    }
-    updatedisplay() {
-        this.inputval.innerText = this.input_value;
-
-    }
+    
+    
 }
-const numbuttons = document.querySelectorAll('[data-number]');
-console.log(numbuttons)
-
-const opreatbtn = document.querySelectorAll('[data-opreation]')
-
-const equalbtn = document.querySelector('[data-equals]')
-
-const clearbtn = document.querySelector('[data-all-clear]')
-
-const inputval = document.querySelectorAll('[data-input]')
-console.log(inputval)
-
-document.addEventListener('DOMContentLoaded', () => {
-    const calc = new calculate(
-        inputval
-    );
-    numbuttons.forEach((button) => {
-        button.addEventListener('click', ()=> {
-            event.preventDefault();
-            calc.appendnumber(button.innerText)
-            console.log(button.innerText)
-            calc.updatedisplay()
-        })
-    })
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
