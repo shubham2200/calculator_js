f2ec8869dcd51c7877f9ef0f8c071e068ff12844
shubhambhalerao <shubhambhalerao54312@gmail.com>
2022-01-21 18:21:31 +0530

    arry_update

--- calculator.js
+++ calculator.js
@@ -1,20 +1,57 @@
-function display(){
-    let numberbtn = document.getElementsByClassName('btn')
-    let screen = document.getElementById('screen_c')
-    let oprea = document.getElementsByClassName('opre')
+function display(val){
+    let screen_value = document.getElementById('screen_c').value += val
+    
+    
 
     
+}
+document.getElementById('clear').addEventListener('click' , clear)
+function clear(){
+    let clearbtn = document.getElementById('screen_c').value = ''
     
+
+    
+
+}
+document.getElementById('result').addEventListener('click' , result)
+function result(){
+    let ans = '';
+    let screen_value = document.getElementById('screen_c').value
+    // console.log(screen_value);
+    if( screen_value === '' ){
+        document.getElementById('screen_c').value = ''
+        console.log('enter the opre 1st')
+    }
+    else{
+        
+        console.log(screen_value);
+        let arr = [];
+        let conunter = 0;
+        let opreation = [];
+        for (i in screen_value){
+            if(screen_value[i]== '*' || screen_value[i] =='+' || screen_value[i] == '%' || screen_value[i] == '-')
+            {
+                conunter++;
+                arr[conunter] = screen_value[i]
+                console.log(arr,conunter)
+            }
+            else{
+                arr[conunter] += screen_value[i]
+                console.log(arr,conunter)
+            }
+
+            
+        }
+
+
+        
+         
+
+    }
+
 }
 
 
 
 
 
-
-
-
-
-// function calculate(){
-//     calculatorform.screen.value = eval(calculatorform.screen.value)
-// }
